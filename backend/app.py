79c50fa984c0985df2f9c0940f1fe58555d46a16
jtisaac <jtisaac@users.noreply.github.com>
2019-02-23 13:56:49 -0600

    initial server files

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from pymongo import MongoClient
+import pymongo
+from aggregation.currentlocation import *
 app = Flask(__name__)
 
-@app.route("/", method=['PUT'])
+# Connect to the database
+client = MongoClient('localhost', 27017)
 
+# The collection that we will edit
+rooms = db.rooms
+
+@app.route("/<rooomandcoordinate>", method=['PUT'])
+def add_to_database(roomandcoordinate):
+    data = roomandcoordinate
     
 @app.route("/", method=['GET'])
