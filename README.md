c24ae07b5ab8ee0c8a206c7d6cc9848affc22328
aishanipal <aishani.pal@gmail.com>
2019-02-23 16:17:29 -0600

    python initialization files

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Where2Meet, When2Meet.
 - Python v2.7.8+
 - pip
 
-# Run all dependecy commands:
-pip install -r requirements.txt
+# Instructions
+1. Go the the root directory (commonground)
+2. `pip install -r requirements.txt`
+3. `python __init__.py`
--- backend/aggregation/locationfinder.py
+++ backend/aggregation/locationfinder.py
@@ -1,27 +1,29 @@
 import geocoder
 import pandas as pd
 
-df = pd.read_csv("data/common_ground_data.csv", error_bad_lines=False, sep=';')
-location_gps = open("data/location_gps.csv", "w+")
-for col in (df.columns.values):
-    location_gps.write(col + ";")
-location_gps.write("x_coord;y_coord\n")
+def generate_coordinates():
+    print("Reading csv...")
+    # df = pd.read_csv("data/common_ground_data.csv", error_bad_lines=False, sep=';')
+    df = pd.read_csv("backend/aggregation/data/common_ground_data.csv", error_bad_lines=False, sep=';')
+    location_gps = open("backend/aggregation/data/location_gps.csv", "w+")
+    for col in (df.columns.values):
+        location_gps.write(col + ";")
+    location_gps.write("x_coord;y_coord\n")
 
-print("Getting all gps coordinates...")
-for i, row in df.iterrows():
+    print("Getting all gps coordinates...")
+    for i, row in df.iterrows():
 
-    category = df.loc[i,'category']
-    building = df.loc[i, 'name']
-    address = df.loc[i, 'address']
-    g = geocoder.osm(address)
-    
-    try:
-        info_str = category + ";" + building + ";" + address + ";" + str(g.osm['x']) + ";" + str(g.osm['y']) + "\n"
-        location_gps.write(info_str)
-    except:
-        print("There is an invalid address...skipping over...")
+        category = df.loc[i,'category']
+        building = df.loc[i, 'name']
+        address = df.loc[i, 'address']
+        g = geocoder.osm(address)
+        
+        try:
+            info_str = category + ";" + building + ";" + address + ";" + str(g.osm['x']) + ";" + str(g.osm['y']) + "\n"
+            location_gps.write(info_str)
+        except:
+            print("There is an invalid address...skipping over...")
 
-print("Finished getting all gps coordinates...")
+    print("Finished getting all gps coordinates...")
 
-location_gps.close()
-    
+    location_gps.close()    
