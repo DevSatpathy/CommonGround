e8047de843ded9c72c275cbc9b8bde9021b2d5d8
aishanipal <aishani.pal@gmail.com>
2019-02-23 11:33:47 -0600

    generate x and y coordinates csv

--- backend/aggregation/locationfinder.py
+++ backend/aggregation/locationfinder.py
@@ -2,8 +2,23 @@ import geocoder
 import pandas as pd
 
 df = pd.read_csv("data/common_ground_data.csv", error_bad_lines=False, sep=';')
+location_gps = open("data/location_gps.csv", "w+")
+for col in (df.columns.values):
+    location_gps.write(col + ";")
+location_gps.write("x_coord;y_coord\n")
 
-print(df)
-# g = geocoder.osm('1408 W Gregory Dr, Urbana, IL 61801')
-# g.json
-# print(g.osm)
+for i, row in df.iterrows():
+
+    category = df.loc[i,'category']
+    building = df.loc[i, 'name']
+    address = df.loc[i, 'address']
+    g = geocoder.osm(address)
+    
+    try:
+        info_str = category + ";" + building + ";" + address + ";" + str(g.osm['x']) + ";" + str(g.osm['y']) + "\n"
+        location_gps.write(info_str)
+    except:
+        print("invalid address")
+
+location_gps.close()
+    
