a09a5ffb8ce9d983f6a12f0ac2d70826942cf1ee
jtisaac <jtisaac@users.noreply.github.com>
2019-02-23 17:48:13 -0600

    Added to joinroom

--- backend/app.py
+++ backend/app.py
@@ -9,24 +9,47 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 
 # Connect to the database
-db = client.db
+db = client.commonground
 
 # The collection that we will edit
 rooms = db.rooms
 
-@app.route("/joinroom")
-def join_room():
+@app.route("/joinroom", methods=['POST'])
+def joinroom():
     name = request.args.get('name')
-    long = request.args.get('longitude')
-    lat = request.args.get('latitude')
+    if long == -1:
+        
+    long = request.args.get('longitude', type=int)
+    lat = request.args.get('latitude', type=int)
     code = request.args.get('code')
     ip_address = request.args.get('ip_address')
-    info = { "ip": ip_address, "longitude": long, "latitude": lat }
-    rooms.insert_one(info)
-
-@app.route("/createroom")
-def create_room():
+    address = request.args.get('address')
+    current_users = rooms.find_one({ 'code':code}, {'_id': 0, 'name': 0, 'users': 1, 'meeting_loc': 0})
+    for i in range(len(current_users)):
+        if current_users[i]['ip_address'] == ip_address:
+            current_users[i] = { "ip": ip_address,
+                    "name": name,
+                    "x": lat,
+                    "y": long,
+                    "address": address }
+        else:
+            current_users.append({ "ip": ip_address,
+                    "name": name,
+                    "x": lat,
+                    "y": long,
+                    "address": address })
+    rooms.update_one(
+            {"code":code},
+            { "$set":{
+                    "users": current_users
+                    }
+            }
+        )
+        
+@app.route("/createroom", methods=['PUT'])
+def createroom():
     name = request.args.get('name')
     ip_address = request.args.get('ip_address')
     
 @app.route("/results")
+
