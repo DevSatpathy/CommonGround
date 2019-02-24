ab4ef4b4e1a3b7f1b0d0d3b22144dcadc1eb0281
Garv Agarwal <garva2@illinois.edu>
2019-02-23 18:35:22 -0600

    working on createroom

--- backend/app.py
+++ backend/app.py
@@ -42,20 +42,21 @@ def joinroom():
         
 @app.route("/createroom", methods=['PUT'])
 def createroom():
-    name = request.args.get('name')
-    ip_address = request.args.get('ip_address')
+	name = request.args.get('name')
+	room_name = request.args.get('room_name')
+	ip_address = request.args.get('ip_address')
 	x_pos = request.args.get('x')
 	y_pos = request.args.get('y')
 	rooms.insertOne(
-		{"name": 3,
+		{"name": room_name,
 		"code": 3,
-		"users": [3],
+		"users": [{"name": name, "x": x_pos, "y": y_pos, "ip": ip_address}],
 		"meeting_location": {
 			"name": 3,
 			"x": 3,
 			"y": 3}})
 
-    
+
 @app.route("/results/<code>")
 def output_data(code):
 	return rooms.find({"code": code})
