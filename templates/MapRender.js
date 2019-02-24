95af1081d47a9313da6bd466414ca46292d5624c
Snehal <snehals2@illinois.edu>
2019-02-24 00:56:30 -0600

    Create Map.Render.js to render map

---
+++ templates/MapRender.js
@@ -0,0 +1,17 @@
+var x = 40;
+var y = -88;
+var uni = L.map('mapid').setView([40.1164, -88.2434], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmoxOSIsImEiOiJjanNoeHF6dTQwd2VrNGF0N3VpYjBhNmVlIn0.DkwEg5jiRyQFqZ-Xe98o-Q', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(uni);
+
+L.marker([40.1101, -88.2290]).addTo(uni)
+    .bindPopup("Meeting Placeholder").openPopup();
+L.marker([40.1201, -88.2090]).addTo(uni)
+  .bindPopup("Person Placeholder").openPopup();
+L.marker([x, y]).addTo(uni)
+  .bindPopup("Best place to meet");
