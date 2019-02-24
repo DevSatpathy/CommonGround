ca8f2025877f71fb96d31f913f500c2158be6c7c
aishanipal <aishani.pal@gmail.com>
2019-02-23 21:47:34 -0600

    .pyc files

--- backend/aggregation/locationfinder.py
+++ backend/aggregation/locationfinder.py
@@ -6,6 +6,8 @@ def generate_coordinates():
     # df = pd.read_csv("data/common_ground_data.csv", error_bad_lines=False, sep=';')
     df = pd.read_csv("backend/aggregation/data/common_ground_data.csv", error_bad_lines=False, sep=';')
     location_gps = open("backend/aggregation/data/location_gps.csv", "w+")
+    # location_gps = open("data/location_gps.csv", "w+")
+
     for col in (df.columns.values):
         location_gps.write(col + ";")
     location_gps.write("x_coord;y_coord\n")
@@ -19,11 +21,12 @@ def generate_coordinates():
         g = geocoder.osm(address)
         
         try:
-            info_str = category + ";" + building + ";" + address + ";" + str(g.osm['x']) + ";" + str(g.osm['y']) + "\n"
+            info_str = building + ";" + category + ";" + address + ";" + str(g.osm['x']) + ";" + str(g.osm['y']) + "\n"
             location_gps.write(info_str)
         except:
             print("There is an invalid address...skipping over...")
 
     print("Finished getting all gps coordinates...")
 
-    location_gps.close()    
+    location_gps.close()   
+generate_coordinates() 
