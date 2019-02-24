a6ec9276c17c2abf0cce96c5eadde18e07ccb866
jtisaac <jtisaac@users.noreply.github.com>
2019-02-23 18:08:54 -0600

    adding to joinroom

--- backend/app.py
+++ backend/app.py
@@ -17,34 +17,28 @@ rooms = db.rooms
 @app.route("/joinroom", methods=['POST'])
 def joinroom():
     name = request.args.get('name')
-    if long == -1:
-        
-    long = request.args.get('longitude', type=int)
+    lon = request.args.get('longitude', type=int)
     lat = request.args.get('latitude', type=int)
     code = request.args.get('code')
     ip_address = request.args.get('ip_address')
-    address = request.args.get('address')
     current_users = rooms.find_one({ 'code':code}, {'_id': 0, 'name': 0, 'users': 1, 'meeting_loc': 0})
     for i in range(len(current_users)):
         if current_users[i]['ip_address'] == ip_address:
             current_users[i] = { "ip": ip_address,
                     "name": name,
                     "x": lat,
-                    "y": long,
-                    "address": address }
+                    "y": lon
+                    }
         else:
             current_users.append({ "ip": ip_address,
                     "name": name,
                     "x": lat,
-                    "y": long,
-                    "address": address })
-    rooms.update_one(
-            {"code":code},
-            { "$set":{
-                    "users": current_users
-                    }
-            }
-        )
+                    "y": lon
+                    })
+    
+    rooms.update_one({"code":code},{ "$set":{"users": current_users}})
+    rooms.update_one({"code":code},{ "$set":{"users": current_users}})
+                
         
 @app.route("/createroom", methods=['PUT'])
 def createroom():
