6936d0efbc4cdafd104ca7cefdf0de5dee83321c
Garv Agarwal <garva2@illinois.edu>
2019-02-24 00:33:47 -0600

    edited /createroom

--- app.py
+++ app.py
@@ -67,7 +67,10 @@ def createroom():
 		"meeting_location": {
 			"name": 3,
 			"x": 3,
-			"y": 3}})
+			"y": 3}
+        }
+    )
+    return render_template('CreateRoom.html')
 
 
 @app.route("/results/<code>")
